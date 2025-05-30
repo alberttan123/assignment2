18dd23efbf818bb327bf1e124c47fba58c0456e9
alberttan123 <attain938@gmail.com>
2025-05-31 05:09:08 +0800

    Revert "fixed approve/reject functionality"
    This reverts commit 1015f487a626349bed89f1e9c7e5663794002693.

--- src/main/java/com/assignment2/service/RoleAccessControl.java
+++ src/main/java/com/assignment2/service/RoleAccessControl.java
@@ -32,6 +32,8 @@ public class RoleAccessControl {
         )));
 
         roleAccessMap.put("finance_manager", new HashSet<>(Arrays.asList(
+                "approve_pos",
+                "modify_po_quantity_supplier",
                 "process_payments",
                 "generate_financial_reports",
                 "view_pr",
